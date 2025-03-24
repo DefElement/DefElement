05f7462d83336e16bc31ae2b34bcba55565bb3b8
Matthew Scroggs <matthew.w.scroggs@gmail.com>
2025-03-24 08:06:23 +0000

    bempp -> bempp_cl (#413)

--- defelement/implementations/bempp_cl.py
+++ defelement/implementations/bempp_cl.py
@@ -1,12 +1,12 @@
-"""Bempp implementation."""
+"""Bempp-cl implementation."""
 
 import typing
 
 from defelement.implementations.core import Element, Implementation, parse_example
 
 
-class BemppImplementation(Implementation):
-    """Bempp implementation."""
+class BemppClImplementation(Implementation):
+    """Bempp-cl implementation."""
 
     @staticmethod
     def format(string: typing.Optional[str], params: typing.Dict[str, typing.Any]) -> str:
@@ -31,26 +31,26 @@ class BemppImplementation(Implementation):
         Returns:
             Example code
         """
-        out = "import bempp.api"
+        out = "import bempp_cl.api"
         out += "\n"
-        out += "grid = bempp.api.shapes.regular_sphere(1)"
+        out += "grid = bempp_cl.api.shapes.regular_sphere(1)"
         for e in element.examples:
             ref, deg, variant, kwargs = parse_example(e)
             assert len(kwargs) == 0
 
             try:
                 bempp_name, input_deg, params = element.get_implementation_string(
-                    "bempp", ref, deg, variant)
+                    "bempp-cl", ref, deg, variant)
             except NotImplementedError:
                 continue
 
             out += "\n\n"
             out += f"# Create {element.name} degree {deg}\n"
-            out += "element = bempp.api.function_space(grid, "
+            out += "element = bempp_cl.api.function_space(grid, "
             out += f"\"{bempp_name}\", {input_deg})"
         return out
 
-    id = "bempp"
-    name = "Bempp"
+    id = "bempp-cl"
+    name = "Bempp-cl"
     url = "https://github.com/bempp/bempp-cl"
     install = "pip3 install numba scipy meshio\npip3 install bempp-cl"
