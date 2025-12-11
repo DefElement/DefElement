0c1f2f851fd988bba167140ceffcd7841b2f0ebd
Matthew Scroggs <matthew.w.scroggs@gmail.com>
2025-12-11 11:51:05 +0000

    archiving workflows and scripts (#510)
    * archiving workflows and scripts
    
    * " -> '

---
+++ .github/scripts/make_release.py
@@ -0,0 +1,30 @@
+import sys
+from datetime import datetime
+
+import github
+
+tar_gz = sys.argv[-2]
+version = sys.argv[-2]
+access_key = sys.argv[-1]
+
+assert version != "2025.09"  # Prevent release when initally merging
+
+git = github.Github(access_key)
+
+defelement = git.get_repo("DefElement/DefElement")
+branch = defelement.get_branch("main")
+ref = defelement.get_git_ref("heads/main")
+
+release = defelement.create_git_tag_and_release(
+    f"v{version}",
+    f"v{version}",
+    f"v{version}",
+    f"Snapshot of DefElement, {datetime.now().strftime('%d %B %Y')}",
+    branch.commit.sha,
+    "commit",
+)
+
+for asset in release.get_assets():
+    asset.delete_asset()
+
+release.upload_asset(tar_gz)
