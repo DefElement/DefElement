d1534455b9f138afa3b42feb7dd3911c96c00d8b
Matthew Scroggs <matthew.w.scroggs@gmail.com>
2026-03-12 07:48:14 +0000

    add test that number of ( == number of )

---
+++ test/test_pages.py
@@ -0,0 +1,17 @@
+import os
+
+import pytest
+import yaml
+
+dir_path = os.path.dirname(os.path.realpath(__file__))
+pages_path = os.path.join(dir_path, "../pages")
+
+md_files = [p for p in os.listdir(pages_path) if p.endswith(".md")]
+
+
+@pytest.mark.parametrize("p", md_files)
+def test_brackets(p):
+    with open(os.path.join(pages_path, p)) as f:
+        page = f.read()
+
+    assert page.count("(") == page.count(")")
