da9b8ff725fba20ee5ac8cae5dbd15ceb913d9f5
Matthew Scroggs <matthew.w.scroggs@gmail.com>
2026-03-19 18:16:44 +0000

    ruff

--- install_implementations.py
+++ install_implementations.py
@@ -13,9 +13,7 @@ if args.install_type not in ["all", "verification"]:
     raise RuntimeError(f"Unknown install type: {args.install_type}")
 
 for i in implementations.implementations.values():
-    if args.install_type == "all" or (
-        args.install_type == "verification" and i.verification
-    ):
+    if args.install_type == "all" or (args.install_type == "verification" and i.verification):
         lang = i.languages[0] if len(i.languages) == 1 else i.install_language
         assert lang is not None
         cmd = i.install(lang)
